e47d80ea11a384681e7b1e789936bb2b6f34697f
caaby <38215101+caaby@users.noreply.github.com>
2026-04-23 17:25:08 +0800

    Update aliases.psm1

--- Winos/aliases.psm1
+++ Winos/aliases.psm1
@@ -4,8 +4,8 @@ Set-Alias grep Select-String
 Set-Alias which Get-Command
 Set-Alias touch New-Item
 
-function Run-RipGrep {
+function Invoke-RipGrep {
     # default to 'smart-case' searches with '-S'
     & (Get-Command rg -CommandType Application) -S @args
 }
-Set-Alias rg Run-RipGrep
+Set-Alias rg Invoke-RipGrep
